0c6ec4792e9fcd6b23560818b80da501d4263ab7
Noureddine <noureddine_taher@yahoo.com>
2020-07-27 16:20:54 +0200

    user autentication

--- src/main/java/com/example/demo/ServeurGestionStockProduitsApplication.java
+++ src/main/java/com/example/demo/ServeurGestionStockProduitsApplication.java
@@ -1,4 +1,4 @@
-package com.example.ServeurGestionStockProduits;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class ServeurGestionStockProduitsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
 	}
-
 }
